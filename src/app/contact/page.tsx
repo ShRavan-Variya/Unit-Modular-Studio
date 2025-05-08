f16b5a8a76b3d32da987936bca4a5bdf8dd15689
ShRavan-Variya <shravanvariya@gmail.com>
2025-05-08 21:29:28 +0530

    map height changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -76,7 +76,7 @@ export default function ContactUs() {
             ></iframe>
           </div> */}
 
-          <div className="relative w-full h-[350px] overflow-hidden">
+          <div className="relative w-full h-[400px] overflow-hidden">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1933920.286701835!2d73.3099711!3d18.7897706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be8072e85576535%3A0xe82f4b207081c33e!2sUnit%20Modular%20Studio!5e0!3m2!1sen!2sin!4v1746717253299!5m2!1sen!2sin"
               width="100%"
